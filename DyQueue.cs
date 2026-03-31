bccc8c15c205c9738807b2d840f1c7da1be1c431
PYJS123 <73827861+PYJS123@users.noreply.github.com>
2026-03-31 16:49:26 +0530

    Add files via upload

---
+++ DyQueue.cs
@@ -0,0 +1,61 @@
+using System;
+
+namespace DynamicQueue
+{
+    public class DyQueue<T>
+    {
+        Node<T> rear;
+        Node<T> front;
+        int queueSize = 0;
+	
+        public int Length { get => queueSize; }
+
+        public void Enqueue(T data)
+        {
+            Node<T> temp = new Node<T>(data);
+		     if (queueSize == 0)
+		     {
+	 	         rear = temp;
+	   	         front = temp;
+		     }
+		     else
+		     {
+		         rear.next = temp;
+				  rear = temp;
+		     }
+		
+            queueSize++;
+        }
+   
+        public T Dequeue()
+        {
+            if (queueSize == 0) throw new System.Exception("Queue is empty");
+		
+		     T toReturn = front.data;
+		
+		     if (queueSize == 1)
+		     {
+		         rear = front = null;
+		     }
+		     else
+		     {
+		         front = front.next;
+		     }
+		
+		     queueSize--;
+		
+		     return toReturn;
+        }
+    }
+
+    public class Node<T>
+    {
+        public T data { get; }
+        public Node<T> next; // the next node after the current one to be dequeued
+	
+        public Node(T data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace DynamicQueue
+{
+	public class Program
+	{
+	    static public void Main()
+	    {
+
+            DyQueue<string> q = new DyQueue<string>();
+			
+            q.Enqueue("Item1");
+			 Console.WriteLine(q.Length);
+			 Console.WriteLine(q.Dequeue());
+			 Console.WriteLine(q.Length);
+		 	
+		   	 q.Enqueue("Item2");
+			 q.Enqueue("Item3");
+			 Console.WriteLine(q.Length);
+			 while (true)
+			 {
+		 		 try
+				 {
+				     Console.WriteLine(q.Dequeue());
+				 }
+				 catch {break;}
+			 }
+			
+			 q.Enqueue("Item4");
+			 q.Enqueue("Item5");
+			 q.Enqueue("Item6");
+			 Console.WriteLine(q.Length);
+			 while (true) {Console.WriteLine(q.Dequeue());}
+        }
+    }
+}
