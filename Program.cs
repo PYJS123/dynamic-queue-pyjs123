cdbcc42ce125ea739971a58adb2956aaff2e54d2
PYJS123 <73827861+PYJS123@users.noreply.github.com>
2026-03-31 11:39:23 +0000

    Just formatting and other things

--- Program.cs
+++ Program.cs
@@ -1,36 +1,36 @@
-using System;
+﻿using System;
 
 namespace DynamicQueue
 {
 	public class Program
 	{
-	    static public void Main()
-	    {
+		static public void Main(string[] args)
+		{
+            // You can use this to test the queue's functionality
+			DyQueue<string> q = new DyQueue<string>();
 
-            DyQueue<string> q = new DyQueue<string>();
-			
-            q.Enqueue("Item1");
-			 Console.WriteLine(q.Length);
-			 Console.WriteLine(q.Dequeue());
-			 Console.WriteLine(q.Length);
-		 	
-		   	 q.Enqueue("Item2");
-			 q.Enqueue("Item3");
-			 Console.WriteLine(q.Length);
-			 while (true)
-			 {
-		 		 try
-				 {
-				     Console.WriteLine(q.Dequeue());
-				 }
-				 catch {break;}
-			 }
-			
-			 q.Enqueue("Item4");
-			 q.Enqueue("Item5");
-			 q.Enqueue("Item6");
-			 Console.WriteLine(q.Length);
-			 while (true) {Console.WriteLine(q.Dequeue());}
-        }
-    }
+			q.Enqueue("Item1");
+			Console.WriteLine(q.Length);
+			Console.WriteLine(q.Dequeue());
+			Console.WriteLine(q.Length);
+
+			q.Enqueue("Item2");
+			q.Enqueue("Item3");
+			Console.WriteLine(q.Length);
+			while (true)
+			{
+				try
+				{
+					Console.WriteLine(q.Dequeue());
+				}
+				catch { break; }
+			}
+
+			q.Enqueue("Item4");
+			q.Enqueue("Item5");
+			q.Enqueue("Item6");
+			Console.WriteLine(q.Length);
+			while (true) { Console.WriteLine(q.Dequeue()); }
+		}
+	}
 }
